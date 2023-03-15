8d6e178c352895f52b38c4300415e5665f6a08be
Reham Hesham <rehamhesham680@gmail.com>
2023-03-15 12:03:12 +0200

    make mails to order paid and shop activation

---
+++ app/Mail/ShopActivated.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Shop;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ShopActivated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Shop $shop;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Shop $shop)
+    {
+        //
+        $this->shop = $shop;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Shop Activated',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'mail.customer.activated',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
